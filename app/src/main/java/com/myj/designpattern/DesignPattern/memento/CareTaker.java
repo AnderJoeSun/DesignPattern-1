8553c5f143695c473499f27ec125ceb21952fec1
maoyujiao <2357614872@qq.com>
2019-09-16 10:40:21 +0800

    备忘录模式：给用户提供了一种可以恢复状态的机制，可以使用户能够比较方便地回到某个历史的状态       2) 实现了信息的封装，使得用户不需要关心状态的保存细节

--- app/src/main/java/com/myj/designpattern/DesignPattern/memento/CareTaker.java
+++ app/src/main/java/com/myj/designpattern/DesignPattern/memento/CareTaker.java
@@ -6,6 +6,12 @@ import java.util.List;
 /**
  * Created by maoyujiao on 2019/9/16.
  * 存在着保存的历史状态
+ *
+ * //如果只保存一次状态
+ private Memento memento;
+ //对 GameRole 保存多次状态
+ //private ArrayList<Memento> mementos; //对多个游戏角色保存多个状态
+ //private HashMap<String, ArrayList<Memento>> rolesMementos;
  */
 
 public class CareTaker {
