184e5b1ee206f6dff4c0bb369bfb3a9ab617b300
maoyujiao <2357614872@qq.com>
2019-09-17 14:59:40 +0800

    适配器模式：三种，类适配器 对象适配器 和接口适配器，区别是src类以何种方式关联到dest中

--- app/src/main/java/com/myj/designpattern/DesignPattern/adapter/ObjectAdapter/VoltageAdapter.java
+++ app/src/main/java/com/myj/designpattern/DesignPattern/adapter/ObjectAdapter/VoltageAdapter.java
@@ -3,7 +3,10 @@ package com.myj.designpattern.DesignPattern.adapter.ObjectAdapter;
 /**
  * Created by maoyujiao on 2019/8/25.
  * 对象适配器模式
-
+ 1) 基本思路和类的适配器模式相同，只是将Adapter类作修改，不是继承src类，而是持有src类的实例，以解决
+ 兼容性的问题。 即:持有 src 类，实现 dst 类接口，完成 src->dst 的适配
+ 2) 根据“合成复用原则”，在系统中尽量使用关联关系(聚合)来替代继承关系。
+ 3) 对象适配器模式是适配器模式常用的一种
  */
 
 public class VoltageAdapter implements IVoltage5 {
